726c69fd52b9b487aaf25cf2999b70fc7fdc927f
Antoine.C <chaponantoinepro@gmail.com>
2025-03-03 16:11:29 +0100

    add 5.1,5.2

---
+++ objects/5.2.js
@@ -0,0 +1,33 @@
+const prompt = require('prompt-sync')();
+
+let askTvSerie = () => {
+    const serie = {};
+    serie.name = prompt("Entré le nom de votre série préférée:");
+    serie.yearOfProduction = Number(prompt(`Entré l'année de production de votre série:`));
+    serie.castMembersName = [];
+    let addCast = true;
+    while (addCast) {
+        const castMember = prompt(`Entré le nom d'un personne du cast de la sérié ( ou laissez un espace vide pour teminer):`);
+        if (castMember.trim() === ""){
+        addCast = false;
+        } else { serie.castMembersName.push(castMember);
+
+        }
+    }
+    return serie;
+}
+
+let translationJSON = (serie) => {
+    const jsonString = JSON.stringify(serie, null , 2);
+    console.log(jsonString);
+}
+
+const preferSerie = askTvSerie();
+    translationJSON(preferSerie);
+
+
+    let randomizeCast = (tvSerie) => {
+        const shuffledArray = tvSerie.castMembersName.sort((a, b) => 0.5 - Math.random());
+        return shuffledArray;
+    }
+    console.log(randomizeCast(preferSerie));
